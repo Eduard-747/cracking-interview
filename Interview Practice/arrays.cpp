cbf89c52c11bf17ab9105fe64337d7880ba37c92
Eduard-747 <88149958+Eduard-747@users.noreply.github.com>
2021-12-22 20:14:15 +0400

    Add files via upload

---
+++ Interview Practice/arrays.cpp
@@ -0,0 +1,137 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+class arrays {
+public:
+    int solution(std::vector<int> a) {
+        std::vector<int> indvec;
+        std::set<int> s ;
+        bool flag {false} ;
+        for(int i = 0 ; i < a.size() ; ++i)
+        {
+            if(s.find(a[i]) != s.end()) {
+                indvec.push_back(i) ;
+                flag = true ;
+            } else {
+                s.insert(a[i]) ;
+            }
+        }
+        return flag ? a[*std::min_element(indvec.begin(),indvec.end())] : -1 ;
+    }
+
+    std::vector<std::vector<int>> solution(std::vector<std::vector<int>> a) {
+        std::vector<std::vector<int>> tmp(a.size());
+        for(int i = 0; i < a.size(); ++i) {
+            tmp[i].resize(a.size()) ;
+        }
+        for(int i = 0; i < a.size(); ++i) {
+            for(int j = 0; j < a.size(); ++j) {
+                tmp[j][a.size() - i - 1] = a[i][j] ;
+            }
+        }
+        return tmp ;
+    }
+
+    bool solution(std::vector<std::string> crypt, std::vector<std::vector<char>> solution) {
+
+        bool flag = true ;   
+        for(int i = 0 ; i < crypt.size() ; ++i)
+        {
+            std::find_if(solution.begin(),solution.end(),[&](std::vector<char> vec){
+                if(crypt[i][0] == vec[0] && vec[1] == '0' && crypt[i].size() != 1)
+                {
+                    flag = false ;
+                    return true ;
+                } return false ;
+            }) ;
+        }
+        
+        int numberA = 0;
+        for (int i = 0; i < crypt[0].size(); i++) 
+        {
+            std::find_if(solution.begin(),solution.end(),[&](std::vector<char> vec){
+                if(crypt[0][i] == vec[0])
+                {
+                    numberA = numberA * 10 + vec[1] - 48 ;
+                    return true ;
+                } return false ;
+            }) ;
+        }
+        int numberB = 0;
+        for (int i = 0; i < crypt[1].size(); i++) 
+        {
+            std::find_if(solution.begin(),solution.end(),[&](std::vector<char> vec){
+                if(crypt[1][i] == vec[0])
+                {
+                    numberB = numberB * 10 + vec[1] - 48 ;
+                    return true ;
+                } return false ;
+            }) ;
+        }
+        int numberC = 0;
+        for (int i = 0; i < crypt[2].size(); i++) 
+        {
+            std::find_if(solution.begin(),solution.end(),[&](std::vector<char> vec){
+                if(crypt[2][i] == vec[0])
+                {
+                    numberC = numberC * 10 + vec[1] - 48 ;
+                    return true ;
+                } return false ;
+            }) ;
+        }
+        std::cout << numberA << " " << numberB << " " << numberC << "\n" ;
+        return ((numberA + numberB) == numberC) && flag ? true : false ;    
+    }
+
+    char solution(std::string s) {
+        for(int i = 0; i < s.size(); i++){
+            char c = s[i] ;
+            s[i] = '0' ;
+            if(std::find(s.begin(),s.end(),c) == s.end()) 
+                return c;
+            else s[i] = c;
+        }
+        return  '_' ;
+    }
+
+    bool check(std::string &str)
+    {
+        str.erase(std::remove(str.begin(),str.end(),'.'),str.end()) ;
+        for(int i = 0; i < str.size(); i++){
+            char c = str[i] ;
+            str[i] = 'x' ;
+            if(std::find(str.begin(),str.end(),c) != str.end()) 
+                return false ;
+            else str[i] = c;
+        }
+        return true ;
+    }
+
+    bool solution(std::vector<std::vector<char>> grid) {
+        std::string str ;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                str.push_back(grid[i % 3 * 3 + j % 3][i / 3 * 3 + j / 3]) ; 
+            }
+            if(!check(str)) return false;
+            str.clear() ;
+        }
+        
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                str.push_back(grid[i][j]) ; 
+            }
+            if(!check(str)) return false;
+            str.clear() ;
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                str.push_back(grid[j][i]) ; 
+            }
+            if(!check(str)) return false ;
+            str.clear() ;
+        }
+        return true ;
+    }
+} ;
